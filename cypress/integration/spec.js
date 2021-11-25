51de574954736d8a2c1b8bd26ef2b31e2c66847b
Alexandre Geissmann <alex@la-bonaventure.com>
2021-11-25 11:27:38 -0500

    init

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,18 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.intercept(
+      {
+        url: /test/,
+      },
+      req => {
+        req.continue(res => {
+          res.delay = 3000;
+          res.send();
+        });
+      },
+    );
+    cy.visit('http://localhost:8080')
+    cy.document().its('body').should('have.class', 'hello')
   })
 })
